422ce4fc3b3ec55c503a9a1b51474c3cc5eba129
atulsuroshe <atul.suroshe@gmail.com>
2021-12-24 20:43:53 +0530

    welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to jenkins demo
                 </div>
 
                 <div class="links">
